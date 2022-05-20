3aac41a156d410bb0f57928f053fc401d924d0ee
Electrolyte <electrolyte.spdf@outlook.com>
2022-05-20 11:45:34 +0530

    added account loader

---
+++ components/Loader.tsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+	return (
+		<div className="absolute top-52 h-36 w-36 bg-gray-800 border border-gray-600 rounded-full animate-pulse grid place-content-center">
+			<img src="https://cdn.jsdelivr.net/gh/SamHerbert/SVG-Loaders/svg-loaders/oval.svg"></img>
+		</div>
+	);
+}
