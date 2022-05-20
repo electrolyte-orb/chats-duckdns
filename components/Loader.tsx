92be933e75f07dece4cab9f0dc05f4ee52da719c
Electrolyte <electrolyte.spdf@outlook.com>
2022-05-20 12:50:33 +0530

    added account delete and sign out

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,6 +1,6 @@
 export default function Loader() {
 	return (
-		<div className="absolute top-52 h-36 w-36 bg-gray-800 border border-gray-600 rounded-full animate-pulse grid place-content-center">
+		<div className="absolute top-1/3 h-36 w-36 bg-gray-800 border border-gray-600 rounded-full animate-pulse grid place-content-center">
 			<img src="https://cdn.jsdelivr.net/gh/SamHerbert/SVG-Loaders/svg-loaders/oval.svg"></img>
 		</div>
 	);
--- pages/account.tsx
+++ pages/account.tsx
@@ -1,16 +1,33 @@
 import { Container, Loader } from '@components';
 import { auth } from 'lib/firebase';
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import Link from 'next/link';
+import ExternalLinkIcon from '@heroicons/react/outline/ExternalLinkIcon';
+import LogoutIcon from '@heroicons/react/outline/LogoutIcon';
+import TrashIcon from '@heroicons/react/outline/TrashIcon';
+import { deleteUser, signOut } from 'firebase/auth';
 
 export default function Account() {
-	const [user, loading] = useAuthState(auth);
+	const [user, loading, error] = useAuthState(auth);
+	const [externalError, setExternalError] = useState<any>();
+
 	const router = useRouter();
 
 	useEffect(() => {
 		if (loading === false && user === null) router.replace('/');
 	}, [user]);
+
+	function handleSignOut() {
+		signOut(auth).catch(setExternalError);
+	}
+
+	function handleDelete() {
+		if (auth.currentUser) {
+			deleteUser(auth.currentUser).catch(setExternalError);
+		}
+	}
 
 	return (
 		<Container>
@@ -23,9 +40,58 @@ export default function Account() {
 					<Loader />
 				) : (
 					<>
-						<h1 className="text-4xl font-bold text-white">Settings</h1>
-						<section className="text-center">
-							<img src="" alt="" />
+						<section className="my-4">
+							<div className="text-center">
+								<div className=" inline-grid h-32 w-32 bg-gray-900 rounded-full place-content-center ring ring-blue-500 ring-offset-4 ring-offset-black">
+									<img src={user?.photoURL!} className="inline-block" />
+								</div>
+								<h1 className="text-4xl font-bold text-white mt-8">Settings</h1>
+								<h3 className="mt-2 font-medium text-2xl text-white">
+									{user?.displayName}
+								</h3>
+							</div>
+							<div className="w-full h-[0.25px] bg-gray-500 my-8"></div>
+							<div>
+								<h1 className="text-lg font-bold text-white">
+									We are not authorized to change Google Account settings.
+								</h1>
+								<Link href="https://myaccount.google.com/">
+									<button className="bg-blue-500 rounded-xl p-4 flex justify-between items-center w-full text-white font-medium mt-4">
+										Google Account settings
+										<ExternalLinkIcon className="h-4" />
+									</button>
+								</Link>
+								<h1 className="text-lg font-bold text-red-500 mt-8">
+									Sign Out or Delete Account
+								</h1>
+								<div className="grid gap-4 grid-cols-2">
+									<button
+										onClick={handleSignOut}
+										className="bg-gray-500 rounded-xl p-4 flex justify-between items-center w-full text-white font-medium mt-4"
+									>
+										Sign Out
+										<LogoutIcon className="h-4" />
+									</button>
+									<button
+										onClick={handleDelete}
+										className="bg-red-500 rounded-xl p-4 flex justify-between items-center w-full text-white font-medium mt-4"
+									>
+										Delete Account
+										<TrashIcon className="h-4" />
+									</button>
+								</div>
+								{error && (
+									<div className="bg-red-500 text-black p-4 rounded-lg mt-4">
+										Error Occured: {error.message}
+									</div>
+								)}
+
+								{externalError && (
+									<div className="bg-red-500 text-black p-4 rounded-lg mt-4">
+										External Error: {new String(externalError)}
+									</div>
+								)}
+							</div>
 						</section>
 					</>
 				)}
